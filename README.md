03f76eaa2c20569dc0b22e522a4a8caec7c53eec
Edward Wu <bluehome.wu@gmail.com>
2023-04-08 00:51:47 +0800

    Add extras for missi-user 11 KASE2202211CN00MP1 V12.0.1.0.RBHCNBS release-keys
    Signed-off-by: Edward Wu <bluehome.wu@gmail.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# missi-user 11 KASE2202211CN00MP1 V12.0.1.0.RBHCNBS release-keys
+- manufacturer: xiaomiblackshark
+- platform: lahaina
+- codename: kaiser
+- flavor: missi-user
+- release: 11
+- id: KASE2202211CN00MP1
+- incremental: V12.0.1.0.RBHCNBS
+- tags: release-keys
+- fingerprint: blackshark/KSR-A0/kaiser:11/KASE2202211CN00MP1/V11.0.4.0.JOYUI:user/release-keys
+blackshark/KSR-A0/kaiser:11/KASE2202211CN00MP1/V11.0.4.0.JOYUI:user/release-keys
+- is_ab: true
+- brand: blackshark
+- branch: missi-user-11-KASE2202211CN00MP1-V12.0.1.0.RBHCNBS-release-keys
+- repo: blackshark_kaiser_dump
---
+++ twrp-device-tree/blackshark/kaiser/omni_kaiser.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from kaiser device
+$(call inherit-product, device/blackshark/kaiser/device.mk)
+
+PRODUCT_DEVICE := kaiser
+PRODUCT_NAME := omni_kaiser
+PRODUCT_BRAND := blackshark
+PRODUCT_MODEL := SHARK KSR-A0
+PRODUCT_MANUFACTURER := blackshark
+
+PRODUCT_GMS_CLIENTID_BASE := android-blackshark
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="KSR-A0-user 11 KASE2202211CN00MP1 V11.0.4.0.JOYUI release-keys"
+
+BUILD_FINGERPRINT := blackshark/KSR-A0/kaiser:11/KASE2202211CN00MP1/V11.0.4.0.JOYUI:user/release-keys
